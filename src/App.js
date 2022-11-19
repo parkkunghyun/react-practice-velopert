a0afa593c2d5f16e457240f321ec0ded51a94ae4
PARK <rudgus4620@gmail.com>
2022-11-19 23:27:35 +0900

    second

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { useRef, useState } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중.,,')
+  return users.filter(user=> user.active).length
+}
+
 function App() {
   const [inputs, setInputs] = useState({
     username: '',
@@ -48,6 +53,20 @@ function App() {
     });
     nextId.current += 1;
   };
+  const onRemove = id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  };
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+  const count = countActiveUsers(users);
+
   return (
     <>
       <CreateUser
@@ -56,7 +75,8 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자 수 : {count}</div>
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
-function User({user}) {
-    return (
-        <div>
-            <b>{user.username}</b> <span>{user.email}</span>
-        </div>
-    )
-
+function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    console.log('user 값이 설정됨');
+    console.log(user);
+    return () => {
+      console.log('user 가 바뀌기 전..');
+      console.log(user);
+    };
+  }, [user]);
+  return (
+    <div>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  );
 }
-function UserList({users}) {
-    
-    return (
-        <div>
-            {
-                users.map( user => (
-                    <User user={user} key={user.id}></User>
-                ))
-            }
-        </div>
-    )
-    
+
+function UserList({ users, onRemove, onToggle }) {
+  return (
+    <div>
+      {users.map(user => (
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
 }
 
-
-export default UserList
+export default UserList;
